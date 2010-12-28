345467375e3b09a31fecd3b92881d4b31414e92c
riopwg <riopwg@piwigo.org>
2010-12-28 12:37:45 +0000

    Update language IT
    git-svn-id: http://piwigo.org/svn/extensions/ExtendedDescription@8339 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -1,6 +1,6 @@
 <?php
 
 $lang['ExtendedDesc_help'] = 'Usare i tag di Extended Description ...';
-/*TODO*/$lang['This category is redirected to %s'] = 'This category is redirected to %s';
+$lang['This category is redirected to %s'] = 'Questa categoria è reindirizzata verso %s';
 
 ?>
