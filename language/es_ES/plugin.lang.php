97a4ae5ef3a9dfc1220b7f47a516d0af76af9c7a
ddtddt <ddtddt@piwigo.org>
2011-02-19 19:32:45 +0000

    [extensions] - ExtendedDescription - update es_ES thanks to AM
    git-svn-id: http://piwigo.org/svn/extensions/ExtendedDescription@9307 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -1,6 +1,6 @@
 <?php
 
 $lang['ExtendedDesc_help'] = 'Utilizar las balizas de...';
-/*TODO*/$lang['This category is redirected to %s'] = 'This category is redirected to %s';
+$lang['This category is redirected to %s'] = 'Esta categoría está redirigida a %s';
 
 ?>
