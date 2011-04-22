4e99507ed2a3edb8cd23f6e9b63d9acf1937b118
ddtddt <ddtddt@piwigo.org>
2011-04-22 07:04:05 +0000

    [extensions] - ExtendedDescription - update SK thanks to dodo
    git-svn-id: http://piwigo.org/svn/extensions/ExtendedDescription@10560 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -1,6 +1,6 @@
 <?php
 
 $lang['ExtendedDesc_help'] = 'Použiť rozšírený popis kľúčových slov...';
-/*TODO*/$lang['This category is redirected to %s'] = 'This category is redirected to %s';
+$lang['This category is redirected to %s'] = 'Táto kategória je presmerovaná na %s';
 
 ?>
