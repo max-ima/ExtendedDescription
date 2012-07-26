0327bc6befd1d1d91e0ab65c61a3593fb0c59c80
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-26 06:29:46 +0000

    [extended_description] Update cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/ExtendedDescription@17119 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['ExtendedDesc_help'] = 'Používat elementy rozšířeného popisu......';
+$lang['ExtendedDesc_help'] = 'Používat elementy rozšířeného popisu...';
 $lang['This category is redirected to %s'] = 'Tato kategorie je přesměrována na %s';
 
 ?>
