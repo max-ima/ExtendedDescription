22779601fb22ca32a0803ecbd468949571ae40d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-05 11:26:51 +0000

    [extended_description] Update de_DE, thanks to : gsnde
    git-svn-id: http://piwigo.org/svn/extensions/ExtendedDescription@26463 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -7,4 +7,7 @@ $lang['Multilingual descriptions'] = 'Mehrsprachige Beschreibung';
 $lang['Extended descriptions'] = 'Ausführlichere Beschreibung';
 $lang['Insert an album or a photo'] = 'Kategorie oder Foto einfügen';
 
+$lang['Extended Description have been successfully installed. Now you can use all its features in most text boxes of Piwigo.'] = 'Extended Description wurde erfolgreich installiert. Ab jetzt können die Features von Extended Description in den meisten Textfeldern von Piwigo verwendet werden.';
+$lang['Hide elements'] = 'Verstecke Elemente';
+$lang['Redirect elements'] = 'Leite Elemente um';
 ?>
