ed649bf0e5e5f5fba794f230d4cc0d90bafcfc41
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-16 22:35:01 +0000

    [extended_description] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/extensions/ExtendedDescription@29579 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -10,4 +10,6 @@ $lang['Insert an album or a photo'] = 'Inserire un album o una foto';
 $lang['Extended Description have been successfully installed. Now you can use all its features in most text boxes of Piwigo.'] = 'Descrizione Estesa è stata installata correttamente. Ora è possibile utilizzare tutte le caratteristiche nella maggior parte delle caselle di testo di Piwigo.';
 $lang['Redirect elements'] = 'Reindirizza elemento';
 $lang['Hide elements'] = 'Nascondi elementi';
-?>
+$lang['Login link & logged in block'] = 'Link di accesso & registrati in blocco';
+$lang['Insert a carousel'] = 'Inserisci un carosello';
+$lang['Back'] = 'Indietro';
