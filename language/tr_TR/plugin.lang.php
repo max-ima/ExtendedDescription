97fd253185d8ff31b633eb5f5a576679948c3008
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-08 16:55:33 +0000

    [extended_description] Update tr_TR, thanks to LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/ExtendedDescription@30401 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -10,4 +10,6 @@ $lang['Insert an album or a photo'] = 'Bir albüm veya fotoğraf yerleştirme';
 $lang['Hide elements'] = 'Öğeleri gizle';
 $lang['Redirect elements'] = 'Öğeleri yönlendir';
 $lang['Extended Description have been successfully installed. Now you can use all its features in most text boxes of Piwigo.'] = 'Genişletilmiş Açıklamalar (Extended Descriptions) başarıyla yüklendi. Şimdi Piwigo metin kutularının çoğunda tüm özelliklerini kullanabilirsiniz.';
-?>
+$lang['Login link & logged in block'] = 'Oturum açma bağlantısı & blokta oturum';
+$lang['Back'] = 'Geri';
+$lang['Insert a carousel'] = 'Bir atlıkarınca ekleyin';
