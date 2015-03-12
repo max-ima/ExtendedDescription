a61d759c0cbfdeaeaec3c7606e34c9bb2ad6e557
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-03-12 04:58:02 +0000

    Update hu_HU, thanks to samli
    git-svn-id: http://piwigo.org/svn/extensions/ExtendedDescription@31028 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -1,10 +1,12 @@
 <?php
 
 $lang['Use Extended Description tags...'] = 'Bővített leírás címkék használatával...';
-$lang['This category is redirected to %s'] = 'Ez a kategória átirányítva %s';
+$lang['This category is redirected to %s'] = 'Ez az album átirányítva %s';
 
 $lang['Multilingual descriptions'] = 'Használat más nyelveken';
 $lang['Extended descriptions'] = 'További lehetőségek';
-$lang['Insert an album or a photo'] = 'Kategória vagy kép beszúrása';
+$lang['Insert an album or a photo'] = 'Slbum vagy kép beszúrása';
 
-?>
+$lang['Back'] = 'Vissza';
+$lang['Hide elements'] = 'Elemek elrejtése';
+$lang['Redirect elements'] = 'Elemek átirányítása';
