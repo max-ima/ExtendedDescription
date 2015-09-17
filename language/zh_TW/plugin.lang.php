690adbb50554265f0a9cbe1cf95783b01a0f3fa7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-09-17 05:45:19 +0000

    Update zh_TW, thanks to azq1231
    git-svn-id: http://piwigo.org/svn/extensions/ExtendedDescription@31307 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_TW/plugin.lang.php
+++ language/zh_TW/plugin.lang.php
@@ -23,10 +23,12 @@
 $lang['Use Extended Description tags...'] = '使用擴展說明標籤...';
 $lang['This category is redirected to %s'] = '此相簿中將被重定向到 %s';
 
-$lang['Multilingual descriptions'] = '';
-$lang['Extended descriptions'] = '';
-$lang['Insert an album or a photo'] = '';
-$lang['Hide elements'] = '';
-$lang['Redirect elements'] = '';
+$lang['Multilingual descriptions'] = '多語言描述';
+$lang['Extended descriptions'] = '擴展描述';
+$lang['Insert an album or a photo'] = '插入一個相冊或圖片';
+$lang['Hide elements'] = '隱藏元素';
+$lang['Redirect elements'] = '重定向元素';
 $lang['Extended Description have been successfully installed. Now you can use all its features in most text boxes of Piwigo.'] = '擴展說明已成功安裝。現在，您可以使用在Piwigo的大多數文本框裡的所有功能。';
-?>
+$lang['Back'] = '後退';
+$lang['Insert a carousel'] = '插入一個幻燈';
+$lang['Login link & logged in block'] = '登錄鏈接&登錄狀態';
